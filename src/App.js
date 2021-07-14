a9a582e8d0cb7391860acce14fd1ece170b2a8b1
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-07-14 19:19:28 +0530

    Add FormControl to take inputs and create form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import FormControl from './components/FormControl/FormControl';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((jsonResponse) => setUsers(jsonResponse))
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
+  const handleValueChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('hi');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        {users.map((user) => {
+          console.log(user.id);
+          return (
+            <FormControl
+              key={user.id}
+              name="user"
+              label={user.name}
+              id={user.id}
+              type="radio"
+              value={user.id}
+              onChange={handleValueChange}
+            />
+          );
+        })}
+        <FormControl name="title" label="Title" onChange={handleValueChange} />
+        <FormControl name="body" label="Body" onChange={handleValueChange} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/FormControl/FormControl.js
@@ -0,0 +1,21 @@
+import classes from './FormControl.module.css';
+
+const FormControl = (props) => {
+  return (
+    <div className={classes.FormControl}>
+      <label htmlFor={props.name}>{props.label}</label>
+      <input
+        name={props.name}
+        type={!props.type ? 'text' : props.type}
+        id={props.type === 'radio' ? props.id : props.name}
+        placeholder={`Enter ${props.name}`}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        value={props.value}
+      />
+      {props.error && <p>{props.error}</p>}
+    </div>
+  );
+};
+
+export default FormControl;
