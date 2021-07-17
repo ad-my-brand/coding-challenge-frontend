03dd58586cdf3980d2e13a174ee43cd8131764ac
Akash Singh <Aka080@users.noreply.github.com>
2021-07-18 00:29:57 +0530

    netlify render issue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+      <Route exact path='/'><App /></Route>
+      <Route path='*'><Redirect to='/'/></Route>
+      </Switch>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
