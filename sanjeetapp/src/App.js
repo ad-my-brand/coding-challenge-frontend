bea5c69eb44bc2a0678765349f49bc7dad5b6ba5
Anubhav <anubhavm1234@gmail.com>
2021-09-26 22:57:05 +0530

    adding basic layout

---
+++ sanjeetapp/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+const styles = {
+  backgroundColor:'yellow',
+  fontSize :'3em',
+  fontWeight:'bold',
+  boxShadow:'3px 3px 3px green'
+}
+
+function App() {
+
+
+  return (
+    <div className="App" style={styles}>
+  
+       Form Control Assignment 
+
+    </div>
+  );
+}
+
+export default App;
