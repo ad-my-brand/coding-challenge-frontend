bcade1120719e5f6b71849180467c4a4c92649b6
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-12-11 20:39:21 +0600

    all done

--- front-end-challange/src/Component/Form_Control.js
+++ front-end-challange/src/Component/Form_Control.js
@@ -14,23 +14,77 @@ const Form_Control = () => {
 
 
 
+    const { register, handleSubmit, reset, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        console.log(data);
+        reset()
+    }
+
+    console.log(watch("example"));
+
+
+
 
     const handleOnchange = e => {
         setUserName(e.target.value);
     }
     console.log(users);
+    // const handleForm = e => {
+    //    
+    //     console.log();
+    //     e.preventDefault();
+    // }
+    // console.log(handleForm);
     return (
         <>
+
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="userName">User Name: </label>
                 <input placeholder='select user name' onChange={handleOnchange} list="name" className=' py-1 px-2 m-2' /><br />
+                <datalist id="name">
+                    {
+                        users.map(user => (
+                            <Form
+                                key={user.id}
+                                user={user}
+
+                            >
+                            </Form>
+                        ))
+                    }
+                </datalist>
+                <label htmlFor="userId">User Id: </label>
+                {
+                    users.filter(usId => usId.name === userName).map(no => (
+                        <input type="number" name="" id="" className=' py-1 px-2 ms-4 w-25%' key={no.id} value={no.id} {...register("userId")} />))
+                } <br />
                 <label htmlFor="title">Title: </label>
                 <input type="text" name="title" id="" {...register("title", { required: true })} className=' py-1 px-2 ms-5 my-2' required /><br />
+                <div className='mx-auto d-block'>
+                    {
+                        users.filter(usId => usId.name === userName).map(singleUser => (
+                            <Mapp
+                                key={singleUser.id}
+                                user={singleUser.address}
+                            >
 
+                            </Mapp>
+
+                        ))
+                    }
+                </div>
                 <label htmlFor="body">Body: </label>
                 <input type="text" name="body" id="" {...register("body", { required: true })} className=' py-1 px-2 ms-5 my-2' required /><br />
                 <button type="submit" className='btn btn-danger'>Submit</button>
             </form>
+
         </>
     );
 };
---
+++ front-end-challange/src/Component/Mapp.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+    height: '400px',
+    width: '50%',
+    margin: '0 auto'
+};
+
+
+
+const Mapp = ({ user }) => {
+
+    const { geo } = user;
+    let lat = parseInt(geo.lat);
+    let lng = parseInt(geo.lng);
+    const center = {
+        lat: lat,
+        lng: lng
+    };
+    return (
+        <LoadScript
+            googleMapsApiKey="AIzaSyCejVuI2J7QOKO7jmYhsoR24Wn5vddQQJY"
+        >
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                <></>
+            </GoogleMap>
+        </LoadScript>
+    );
+};
+
+export default Mapp;
