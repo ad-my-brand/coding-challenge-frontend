ef286fe2bfdd39c9875c2067bce9d9bd17137c93
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-01 18:43:28 +0530

    added TailwindCSS and configured it

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -11,8 +9,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="">
-
+      <div className=" ">
+        
       </div>
     </>
   )
