51ea1c967901f397238d266f47bc75678b54d75f
Ritik joshi <ritikjoshi688@gmail.com>
2022-01-29 11:22:31 +0530

    test-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Formcontrol from "./component/Formcontrol";
+
+function App() {
+  return (
+    <div className="App">
+      <Formcontrol />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Formcontrol.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Formcontrol() {
+  return (
+    <div>
+      <h1>Hellow</h1>
+    </div>
+  );
+}
+
+export default Formcontrol;
