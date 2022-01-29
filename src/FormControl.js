9bda3c511d25ba0df1d028d40b1b1913f1c7fdcf
Yash Chaudhari <yashchaudhari008@gmail.com>
2022-01-29 23:42:47 +0530

    Simple App

---
+++ src/FormControl.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function FormControl({ users, handleChange }) {
+  return (
+    <form className="controlForm">
+      <label htmlFor="users">Select User: </label>
+      <select name="users" onChange={handleChange}>
+        <option value="0">none</option>
+        {users.map((user) => {
+          return (
+            <option value={user.id} key={user.id}>
+              {user.name}
+            </option>
+          );
+        })}
+      </select>
+    </form>
+  );
+}
