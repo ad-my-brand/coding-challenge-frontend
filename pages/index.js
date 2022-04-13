a1d267520b506e5819765f26639de4a8c9c217b9
sulaksh <sulaksh3008@gmail.com>
2022-04-13 17:33:35 +0530

    Completion of coding assignment

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Container from '../layout/Container';
+import Header from '../layout/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Container />
+    </>
+  );
+}
