1a882d03a8c4a8dd8b6e3dfcf4e6431dd98867b2
lB4Ul <anish.sarkar.official@gmail.com>
2022-05-11 09:35:38 +0530

    Changes made

--- src/components/MapComp.js
+++ src/components/MapComp.js
@@ -1,30 +1,13 @@
-// import { Feature } from 'ol'
-// import { Point } from 'ol/geom'
 import { fromLonLat } from 'ol/proj'
-// import { Vector } from 'ol/source';
 import React, { useEffect, useRef, useState } from 'react'
 import { layer, custom, Map, Layers } from "react-openlayers"
 
 const MapComp = ({lat, lng}) => {
-    // var iconFeature = new Feature(new Point([0, 0]));
-    // const source = new Vector({features: [iconFeature]});
-    // const marker = new custom.style.MarkerStyle(
-    //     'https://openlayers.org/en/v4.0.1/examples/data/icon.png'
-    //   );
-
-    // useEffect(() => {
-    //   console.log(lng)
-    
-
-    // })
-    
-
     return (
         <div className='Map'>
             <Map view={{ center: fromLonLat([lng, lat]), zoom: 10 }}>
                 <Layers>
                     <layer.Tile />
-                    {/* <layer.Vector source={source} style={marker.style} zIndex="1" /> */}
                 </Layers>
             </Map>
         </div>
