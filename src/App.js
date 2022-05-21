37ae69c5e2e943b092b27646df54a33d8a8f6643
Vaibhav <vaibhavpandeyprayag@gmail.com>
2022-05-21 11:43:01 +0530

    Assignment commited

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MyForm from "./component/MyForm";
+import styles from "./app.module.css";
+function App() {
+  return (
+    <div className={styles.app}>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
