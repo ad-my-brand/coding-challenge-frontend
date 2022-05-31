e2e3877141af782c783c61bb71342faa222819bc
Dani-Hacker <danieljebarson21@gmail.com>
2022-05-31 17:39:09 +0530

    created basic Map component

---
+++ components/Map.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+class Map extends Component {
+  c;
+  render = () => {
+    const GOOGLE_MAP_KEY = process.env.GOOGLE_MAP_KEY;
+    const containerStyle = {
+      width: "400px",
+      height: "400px",
+    };
+
+    const center = {
+      lat: -3.745,
+      lng: -38.523,
+    };
+    return (
+      <LoadScript googleMapsApiKey={GOOGLE_MAP_KEY}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    );
+  };
+}
+
+export default Map;
