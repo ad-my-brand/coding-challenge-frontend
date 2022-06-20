105a92c518257ae59ba17cee73426dfdc01d6363
aquila-shivam <shivam.52.nishad@gmail.com>
2022-06-20 07:23:13 +0000

    shivam

--- components/formComponent.js
+++ components/formComponent.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { fetchData } from '../utils/fetchData'
+
+
 const formComponent = () => {
+  
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    const fetchUserData= async()=>{
+      const userData = await fetchData('https://jsonplaceholder.typicode.com/users');
+      setUser(userData);
+    }
+     
+    fetchUserData();
+    
+}, [])
+
+const handleSubmit=()=>{
+
+}
+
+console.log(user);
   return (
-    <div>hello</div>
+    <form>
+    <select>{user.map((data)=>(<option key ={data.id} value={data.username}>{data.username}</option>))}</select>
+    <input type='submit'/>
+    </form>
   )
 }
 
