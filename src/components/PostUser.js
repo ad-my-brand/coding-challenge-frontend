393b9d3996cedce839b95a20723a668e318e8376
Sebin CM <sebinsebzz2002@gmail.com>
2022-06-29 18:38:10 +0530

    Remove comments

--- src/components/PostUser.js
+++ src/components/PostUser.js
@@ -157,34 +157,3 @@ const ErrorText = styled.p`
   font-size: 0.9em;
   font-family: -apple-system, BlinkMacSystemFont, "Roboto", sans-serif;
 `;
-
-// <!-- HTML !-->
-// <button class="button-31" role="button">Button 31</button>
-
-// /* CSS */
-// .button-31 {
-//   border-radius: 4px;
-//   border-style: none;
-//   box-sizing: border-box;
-//   color: #fff;
-//   cursor: pointer;
-//   display: inline-block;
-//   font-family: "Farfetch Basis","Helvetica Neue",Arial,sans-serif;
-//   font-size: 16px;
-//   font-weight: 700;
-//   line-height: 1.5;
-//   margin: 0;
-//   max-width: none;
-//   min-height: 44px;
-//   min-width: 10px;
-//   outline: none;
-//   overflow: hidden;
-//   padding: 9px 20px 8px;
-//   position: relative;
-//   text-align: center;
-//   text-transform: none;
-//   user-select: none;
-//   -webkit-user-select: none;
-//   touch-action: manipulation;
-//   width: 100%;
-// }
