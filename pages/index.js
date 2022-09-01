333b8a4172a234bc531d578f5d4ac5d23342cc69
Im-Abhi <abhishekgupta942001@gmail.com>
2022-09-01 17:14:16 +0530

    remove getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -21,13 +21,3 @@ export default function Home() {
     </div>
   )
 }
-
-export async function getStaticProps() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await response.json();
-  return {
-    props: {
-      users: data
-    }
-  }
-}
