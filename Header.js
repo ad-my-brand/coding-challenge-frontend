21df2ce33693ff44942d1cd48cc9f6a4fc84e6e9
Mohd Huzaifa <mohdhuzaifa80@gmail.com>
2022-09-05 13:24:51 +0530

    FormControl and Header

---
+++ Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+ return(
+    <div className='header'>
+        <p>
+        <h1>
+            Coding Challenge Front End
+        </h1>
+            <br/>
+        <h3>
+            By:- Mohd Huzaifa
+        </h3>
+        </p>
+    </div>
+ );
+}
+export default Header;
