23d99d3b6a9e57f05a6675450fea25b3ad9e2197
Krishna47ak <anandakrishna720@gmail.com>
2023-09-02 17:42:03 +0530

    Added loading animation

---
+++ src/components/Spinner.js
@@ -0,0 +1,7 @@
+import spinner from '../images/spinner2.gif'
+
+export default () => (
+    <section>
+        <img src={spinner} style={{ width: '100px', margin: 'auto', display: 'block' }} alt='Loading...' />
+    </section>
+)
